a5d810b1e46dae361e63dcb37b3e507fb3cb7188
Alexander <alex@alexvoiceover.com>
2024-02-11 09:29:01 +0000

    Updated index for AWS secrets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-require('dotenv').config()
-
 const server = require('./server.js')
 
-const PORT = process.env.PORT || 8080
+const PORT = 8080
 const HOST = '0.0.0.0' // Listen on all network interfaces
 
 server.listen(PORT, HOST, () => {
