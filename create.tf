31d522179d4fcd45f10e58b5ac5da05018ac72d4
Alexander <alex@alexvoiceover.com>
2024-02-13 09:06:54 +0000

    Created waiting page

--- create.tf
+++ create.tf
@@ -23,6 +23,13 @@ user_data = <<-EOF
 sudo yum update -y
 sudo yum install git -y
 
+# Install a simple web server to serve the static index.html
+sudo yum install -y httpd
+# Start the web server
+sudo service httpd start
+# Enable httpd to start on boot
+sudo systemctl enable httpd
+
 # Install NVM as the ec2-user
 sudo -u ec2-user bash <<'EOF2'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
@@ -51,6 +58,9 @@ npm install -g npm@latest
 mkdir -p $HOME/ServerDB-ZPAO
 git clone -b AWSSecrets https://github.com/FAC29A/ServerDB-ZPAO.git $HOME/ServerDB-ZPAO
 cd $HOME/ServerDB-ZPAO
+# Copy the index.html from your project to the web server's document root
+cp index.html /var/www/html/index.html
+
 npm install
 npm run start
 # npm install -g pm2
@@ -87,6 +97,14 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Port (80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all outbound traffic
   egress {
     from_port   = 0
@@ -100,9 +118,13 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+output "waiting_page" {
+  value = "http://${aws_instance.app_instance.public_ip}/"
+  description = "URL to access the waiting "
+}
+
 output "app_url" {
   value = "http://${aws_instance.app_instance.public_ip}:8080/"
   description = "URL to access the application"
 }
-
 
